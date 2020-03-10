a9698203798e5e45d816358ee3d2ac43e63fcb7b
Jakub Zych <jzych89@gmail.com>
2020-03-10 11:16:58 +0100

    Add LogLevel enum

--- inc/Log.hpp
+++ inc/Log.hpp
@@ -1,12 +1,20 @@
 #pragma once
 #include <queue>
+#include <string>
+
+enum class LogLevel {
+    info,
+    debug,
+    error
+};
 
 struct LogRecord {
+    LogLevel lvl;
     std::string data;
 };
 
 struct Log {
     std::queue<LogRecord> queue;
 
-    void addRecod(LogRecord rec);
+    void addRecord(LogRecord rec);
 };

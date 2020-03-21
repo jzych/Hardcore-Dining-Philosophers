ba0aab44f44d6cdf81b177b8fc6f1eb7d5d072d8
jzych <46497556+jzych@users.noreply.github.com>
2020-03-21 20:20:41 +0100

    Logger implementation (#20)
    * Add LogLevel enum
    
    * Logger implementatnion
    
    * Log: switch queue to deque
    
    * Log: add function to add events with proper levels
    
    * Log: define constructor and destructor

--- inc/Log.hpp
+++ inc/Log.hpp
@@ -1,12 +1,39 @@
 #pragma once
-#include <queue>
+#include <deque>
+#include <string>
+#include <mutex>
+#include <condition_variable>
 
-struct LogRecord {
+#ifndef DEBUG
+constexpr bool debug = false;
+#else
+constexpr bool debug = true;
+#endif
+
+enum class LogLevel {
+    info,
+    debug,
+    error
+};
+
+struct LogEvent {
+    LogLevel lvl;
     std::string data;
 };
 
-struct Log {
-    std::queue<LogRecord> queue;
+class Log {
+    std::mutex m_;
+    std::condition_variable nonEmpty_;
+    std::deque<LogEvent> queue_;
+    
+    std::string addLevelFlag(LogLevel lvl); // Returns prefix string for proper log level
+    void addEvent(LogEvent rec);            // Adds event to queue
+public:
+    Log();
+    ~Log();
 
-    void addRecod(LogRecord rec);
+    void processEvent();                    // Takes log messege from queue and prints it
+    void logInfo(std::string text);         // Shows progress of execution
+    void logDebug(std::string text);        // Shows additional data for debugging
+    void logError(std::string text);        // Show errors messeges
 };
--- src/Log.cpp
+++ src/Log.cpp
@@ -1,1 +1,64 @@
-#include "Table.hpp"
+#include "Log.hpp"
+#include <iostream>
+#include <sstream>
+
+Log::Log() {
+    logInfo("Dinner begins");
+}
+
+Log::~Log() {
+    logInfo("Dinner ends");
+}
+
+
+std::string Log::addLevelFlag(LogLevel lvl) {
+    if(lvl == LogLevel::error)
+        return "ERROR: ";
+
+    if(debug) {
+        if(lvl == LogLevel::info)
+            return "INFO:  ";
+        if(lvl == LogLevel::debug)
+            return "DEBUG: ";
+    }
+
+    return "";
+}
+
+void Log::addEvent(LogEvent rec) {
+    std::unique_lock<std::mutex> l(m_);
+    queue_.push_front(rec);
+    nonEmpty_.notify_all();
+}
+
+void Log::processEvent() {
+    std::unique_lock<std::mutex> l(m_);
+    auto hasData = [&]{ return not queue_.empty(); };
+    nonEmpty_.wait(l, hasData);
+
+    std::stringstream output;
+    auto record = queue_.back();
+    queue_.pop_back();
+
+    output << addLevelFlag(record.lvl);
+    output << record.data << "\n";
+
+    std::cout << output.str();
+}
+
+void Log::logInfo(std::string text) {
+    LogEvent evt{LogLevel::info, text};
+    addEvent(evt);
+}
+
+void Log::logDebug(std::string text) {
+    if(debug) {
+        LogEvent evt{LogLevel::debug, text};
+        addEvent(evt);
+    }
+}
+
+void Log::logError(std::string text) {
+    LogEvent evt{LogLevel::error, text};
+    addEvent(evt);
+}
